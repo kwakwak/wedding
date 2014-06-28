2d8a9c9a756eff75becf1a28608fd3f3de1e5e2e
roni <ronihcohen@gmail.com>
2014-06-28 20:21:28 +0300

    total pepole

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -4,6 +4,14 @@ angular.module('weddingApp')
   .controller('ListCtrl', ['$scope','resource',function ($scope, resource) {
         resource.query(function(invites) {
             $scope.invites = invites;
+            // count total group size
+            $scope.group_size_sum = 0;
+            for (var i in $scope.invites) {
+                if ($scope.invites[i].number){
+                    $scope.group_size_sum+= $scope.invites[i].number;
+                }
+            }
+
         });
 
         $scope.remove = function (invite){
