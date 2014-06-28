e7320a770907213f35b43e83ab645d05247a6250
Roni <ronihcohen@gmail.com>
2014-06-28 13:13:59 +0300

    remove-  feature

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -4,5 +4,16 @@ angular.module('weddingApp')
   .controller('ListCtrl', ['$scope','resource',function ($scope, resource) {
         resource.query(function(invites) {
             $scope.invites = invites;
-    });
+        });
+
+        $scope.remove = function (invite){
+
+            invite.$remove({id:invite._id},function(res){
+                for (var i in $scope.invites) {
+                    if ($scope.invites[i]._id === res._id) {
+                        $scope.invites.splice(i, 1);
+                    }
+                }
+            });
+        }
   }]);
