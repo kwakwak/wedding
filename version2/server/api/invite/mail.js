7c9bed4fb5d9df4a4d02f189e83b0b08c447c2ea
Roni <ronihcohen@gmail.com>
2014-10-16 10:15:50 +0300

    change to icloud

--- version2/server/api/invite/mail.js
+++ version2/server/api/invite/mail.js
@@ -7,10 +7,10 @@ exports.send = function(req){
 
 // create reusable transporter object using SMTP transport
     var transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: 'icloud',
         auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASS
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
         }
     });
 
@@ -20,7 +20,7 @@ exports.send = function(req){
 // setup e-mail data with unicode symbols
 
     var mailOptions = {
-        from: 'Roni HaCohen <ronihcohen@gmail.com>', // sender address
+        from: 'Roni HaCohen <ronihcohen@icloud.com>', // sender address
         to: 'ronihcohen@gmail.com, paulilog@gmail.com', // list of receivers
         subject: req.body.name + ' confirms wedding invite ', // Subject line
         text: req.body.name + ' confirms wedding invite ' // plaintext body
@@ -29,7 +29,8 @@ exports.send = function(req){
 //send mail with defined transport object
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
-            console.log(error +' user: '+ process.env.GMAIL_USER + ' pass: ' + process.env.GMAIL_PASS);
+            console.log(error);
+            console.log('user: '+ process.env.MAIL_USER + ' pass: ' + process.env.MAIL_PASS);
         }else{
             console.log('Message sent: ' + info.response);
         }
